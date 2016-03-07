595d12c900f673b041d44a0c1de59f2993e6665e
hokashao <kejiashao@gmail.com>
2016-03-07 15:25:46 -0800

    adding the content generator plugin

---
+++ data/KA Titles/grammar.js
@@ -0,0 +1,10 @@
+data = [
+"Concrete and abstract nouns ",
+"Irregular Plural Nouns, Part IV",
+"Common and proper nouns",
+"Irregular Plural Nouns, Part II",
+"Irregular Plural Nouns, Part III",
+"Irregular Plural Nouns, Part I",
+"Introduction to singular and plural nouns",
+"BONUS VIDEO \u2013 Origin of the Mutant Plural",
+"Introduction to nouns"]
